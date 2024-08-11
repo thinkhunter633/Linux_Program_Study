e27010cea7c247428fcb0ecf5bfdb1aaa21233f3
thinkhunter633 <122496041@qq.com>
2024-08-11 16:18:38 +0800

    adds news files

---
+++ ubuntu_socket/frame_procedure.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+
+int32_t frame_procedure(char* buf, int32_t len)
+{
+    // TODO: implement frame procedure
+    return len;
+}
---
+++ ubuntu_socket/frame_procedure.h
@@ -0,0 +1,6 @@
+#ifndef __FRAME_PROCEDURE_H__
+#define __FRAME_PROCEDURE_H__
+
+int32_t frame_procedure(char* buf, int32_t len);
+
+#endif
