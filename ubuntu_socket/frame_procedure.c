8269c5a3e9a4afe30564b5241e5c1a663139601f
thinkhunter633 <122496041@qq.com>
2024-08-13 16:12:55 +0800

    feat:adds http_register_uri table

--- ubuntu_socket/frame_procedure.c
+++ ubuntu_socket/frame_procedure.c
@@ -14,6 +14,26 @@ typedef struct http_message_tag{
 
 http_message g_xHttpMessage = {0};
 
+int32_t http_register_uri_GET_index(const char* uri)
+{
+    /*@todo, procedure index.html*/
+    
+    return 0;
+}
+typedef struct http_uri_table_tag {
+    enum http_method method;
+    const char* uri;
+    int32_t (*handler)(const char* uri);
+}http_uri_table;
+
+http_uri_table g_xHttpUriTable[] = {
+    {HTTP_GET,"/index.html",http_register_uri_GET_index},
+};
+
+
+
+
+
 static int on_info(http_parser* p) {
   return 0;
 }
@@ -23,28 +43,19 @@ static int on_data(http_parser* p, const char *at, size_t length) {
   return 0;
 }
 
-int request_url_cb(http_parser* p, const char* buf, size_t len)
-{
+int request_url_cb(http_parser* p, const char* buf, size_t len) {
     strncat(g_xHttpMessage.request_url, buf, len);
     printf("request_url_cb: %d, %s\n", (int)len, g_xHttpMessage.request_url);
     return 0;
 }
 
 
-int headers_complete_cb(http_parser* p)
-{
-	// assert(p == parser);
-	// messages[num_messages].method = (http_method)parser->method;
-	// messages[num_messages].status_code = parser->status_code;
-	// messages[num_messages].http_major = parser->http_major;
-	// messages[num_messages].http_minor = parser->http_minor;
-	// messages[num_messages].headers_complete_cb_called = true;
-	// messages[num_messages].should_keep_alive = http_should_keep_alive(parser);
+int headers_complete_cb(http_parser* p) {
     g_xHttpMessage.method = p->method;
     g_xHttpMessage.http_major = p->http_major;
     g_xHttpMessage.http_minor = p->http_minor;
     g_xHttpMessage.keepalive_flag = http_should_keep_alive(p);
-	return 0;
+    return 0;
 }
 
 int message_complete_cb(http_parser* p) {
@@ -54,17 +65,16 @@ int message_complete_cb(http_parser* p) {
 }
 
 static http_parser_settings settings = {
-  .on_message_begin = on_info,
-  .on_headers_complete = on_info,
-  .on_message_complete = on_info,
-  .on_headers_complete = headers_complete_cb,
-  .on_header_field = on_data,
-  .on_header_value = on_data,
-  .on_url = request_url_cb,
-  .on_status = on_data,
-  .on_body = on_data
+    .on_message_begin = on_info,
+    .on_headers_complete = on_info,
+    .on_message_complete = on_info,
+    .on_headers_complete = headers_complete_cb,
+    .on_header_field = on_data,
+    .on_header_value = on_data,
+    .on_url = request_url_cb,
+    .on_status = on_data,
+    .on_body = on_data
 };
-
 
 
 int32_t frame_procedure(char* buf, int32_t buf_len)
